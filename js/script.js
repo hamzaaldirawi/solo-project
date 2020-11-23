629ec06e7c879c84d50aff61cf6f60ba026b7100
hamzaaldirawi <hamza.dirawie@gmail.com>
2020-11-24 02:18:38 +0300

    Add Carousel to Team section and mouse wheel for it

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
-
+/* Preloader */
 $(window).on('load', function() {
     //Make sure whole website is fully loaded
     $('#preloader-holder').delay(350).fadeOut('slow');
+});
+
+/* Owl Carousel - Team */
+var owl = $('#team-members');
+
+$(function() {
+    owl.owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+    });
+});
+
+$(function() {
+    owl.on('mousewheel', '.owl-stage', function (e) {
+        if (e.deltaY>0) {
+            owl.trigger('next.owl');
+        } else {
+            owl.trigger('prev.owl');
+        }
+        e.preventDefault();
+    });
 });
